e32f92927a97cd10ed01b664ddc01b8372ab6f42
Sravanthi Konduru <srkonduru@paypal.com>
2015-05-06 10:38:51 -0500

    disable test execution

--- run-travis-ci-tests.sh
+++ run-travis-ci-tests.sh
@@ -4,5 +4,5 @@ if [ -n "${SAUCE_USERNAME}" ]; then
   echo { \"sauceUserName\": \"${SAUCE_USERNAME}\", \"sauceApiKey\": \"${SAUCE_ACCESS_KEY}\", \"tunnel-identifier\": \"__string__${TRAVIS_JOB_NUMBER}\", \"build\": \"${TRAVIS_BUILD_NUMBER}\", \"idle-timeout\": 120, \"tags\": [\"commit ${TRAVIS_COMMIT}\", \"branch ${TRAVIS_BRANCH}\", \"pull request ${TRAVIS_PULL_REQUEST}\"] } > client/src/test/resources/sauceConfig.json
   mvn test -Djava.net.preferIPv4Stack=true -DSELION_BROWSER_CAPABILITY_PLATFORM=WINDOWS
 else
-  mvn test -Djava.net.preferIPv4Stack=true -DsuiteXmlFile=PhantomJS-Suite.xml -DSELION_SELENIUM_RUN_LOCALLY=true
+  mvn test -DskipTests=true -Djava.net.preferIPv4Stack=true -DsuiteXmlFile=PhantomJS-Suite.xml -DSELION_SELENIUM_RUN_LOCALLY=true
 fi
