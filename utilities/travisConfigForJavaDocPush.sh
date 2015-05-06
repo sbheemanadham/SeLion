7c56035e7915920d02d1a9cab67b6717b96b1877
Sravanthi Konduru <srkonduru@paypal.com>
2015-05-06 10:30:22 -0500

    updated scripts

--- utilities/travisConfigForJavaDocPush.sh
+++ utilities/travisConfigForJavaDocPush.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
+#Generate the javadocs only if the following conditions are satisfied
+#TRAVIS_REPO_SLUG - only run the script on the original project, not on the fork
+#TRAVIS_JDK_VERSION - Use oraclejdk7 to generate javadocs
+#TRAVIS_PULL_REQUEST - Check the build is not against a pull request
+#TRAVIS_BRANCH - Check the build is against 'develop' branch
 if [ "$TRAVIS_REPO_SLUG" = "sbheemanadham/SeLion" ] && [ "$TRAVIS_JDK_VERSION" = "oraclejdk7" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ "$TRAVIS_BRANCH" = "develop" ]; then
-	echo -e "Publishing javadoc...\n"
+	echo -e "Publishing javadocs...\n"
 	cd $HOME
+	#update user email and name in git config
 	git config --global user.email "travis@travis-ci.org"
 	git config --global user.name "travis-ci"
+	# clone the project using TOKEN
 	git clone --quiet https://${GH_TOKEN}@github.com/sbheemanadham/SeLion > /dev/null
 	cd SeLion
 	sh utilities/push-javadoc-to-gh-pages.sh
 	echo -e "Published Javadoc to gh-pages branch.\n"
 else
-	echo -e "Failed to publish Javadocs to gh-pages branch.\n"
+	echo -e "Failed to publish Javadocs to gh-pages branch as one or more validations against Travis environment variables failed.\n"
 fi
